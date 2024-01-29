dc95a9926120220b1e583753e9d08ce28e03265d
santhoshtk08 <santhoshtk2001@gmail.com>
2024-01-29 20:32:12 +0530

    Finished BE

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const zod = require("zod");
 const jwt = require("jsonwebtoken");
-const { User } = require("../db");
+const { User, Account } = require("../db");
 const { JWT_SECRET } = require("../config");
 const { authMiddleware } = require("../middleware");
 const router = express.Router();
@@ -41,6 +41,11 @@ router.post("/signup", async (req, res) => {
     })
 
     const userId = user._id
+
+    await Account.create({
+        userId,
+        balance: 1 + Math.random() * 10000
+    })
 
     const token = jwt.sign({ userId }, JWT_SECRET)
 
